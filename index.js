19c1c59cc8448700a6ece42eaa1875fb95c26189
dan <dannykhan614@gmail.com>
2024-03-20 04:24:21 +0300

    complete arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+let multiply = num1 * num2;
+let num3 = 14;
+let num4 = 5;
+
+let mod = num3 % num4;
+let numbers = (17,19,20,18)
+let max = Math.max(17,19,20,18)
+let random = Math.floor(Math.random() * 100) + 1;
+
